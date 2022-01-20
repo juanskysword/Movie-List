e0552b62e77896459e21048cf000afb8826d6c0e
Juan Tavera <juanskysword@gmail.com>
2022-01-19 16:19:37 -0800

    stuckaddandremove

---
+++ index.js
@@ -0,0 +1,30 @@
+// console.log('Hello World')
+
+function addMovie (event) {
+const inputField = document.querySelector(".input");
+const movie = movie.createElement(".li");
+
+const movieTitle = document.createElement(".pan");
+movieTitle.appendChild(".li");
+const ul = document.querySelector(".ul");
+movieTitle.appendChild("movie");
+
+
+const delbtn =document.createElement("button");
+delbtn.textContent(X);
+delbtn.addEventListener("click",(deletebutton);
+delbtn.appendChild('movie');
+}
+    
+function deletebutton (event){
+    delbtn.appendChild(".li");
+
+}
+
+var form = document.querySelector("form");
+
+form.addEventListener("click",(); 
+    console.log(evt)
+
+
+addMovie()
